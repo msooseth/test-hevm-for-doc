31b13ef3d4ce9d0028ce17b20222cd55a3e758ae
Mate Soos <mate.soos@ethereum.org>
2024-01-25 14:47:59 +0100

    My stuff

--- todo.sh
+++ todo.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
+forge init
 forge clean
 forge build --extra-output ir
-HEVM=/home/matesoos/development/hevm/dist-newstyle/build/x86_64-linux/ghc-9.4.5/hevm-0.51.3/x/hevm/build/hevm/hevm
-# $HEVM test --match "contract.sol.*"
-$HEVM test --match 'src/contract.sol.*prove_add_value' --solver z3
+hevm test --match 'src/contract.sol.*prove.*' --solver z3
