0b65dc71da8caf13e95efd2d019312cbfc01069a
Mate Soos <mate.soos@ethereum.org>
2024-02-12 21:08:24 +0100

    Fixing up contract

--- src/contract.sol
+++ src/contract.sol
@@ -1,5 +1,8 @@
 pragma solidity ^0.8.19;
-contract MyContract {
+
+import {Test} from "forge-std/Test.sol";
+
+contract MyContract is Test {
   mapping (address => uint) balances;
   function prove_add_value(address recv, uint amt) public {
     require(balances[recv] <= 100);
